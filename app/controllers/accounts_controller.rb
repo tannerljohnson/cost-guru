43319a01180e4f21d0aa78dd8d1d8a8c17b0760e
Tanner Johnson <tannerj@makenotion.com>
2023-12-29 08:11:49 -0800

    reformat controllers

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,85 +1,85 @@
 class AccountsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def index
-        @accounts = current_user.accounts
+  def index
+    @accounts = current_user.accounts
+  end
+
+  def new
+    @account = current_user.accounts.new
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+    if @account.save
+      redirect_to account_analyses_path(@account)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @account = current_user.accounts.new
+  def edit
+    @account = current_user.accounts.find { |account| account.id == params[:id] }
+    raise "Account not found" unless @account
+  end
+
+  def update
+    @account = current_user.accounts.find { |account| account.id == params[:id] }
+
+    if @account.update(account_params)
+      redirect_to account_analyses_path(@account)
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def create
-        @account = current_user.accounts.new(account_params)
-        if @account.save
-          redirect_to account_analyses_path(@account)
-        else
-          render :new, status: :unprocessable_entity
-        end
-    end
+  def show
+    @account = current_user.accounts.find { |account| account.id == params[:id] }
+    raise "Account not found" unless @account
 
-    def edit
-        @account = current_user.accounts.find { |account| account.id == params[:id] }
-        raise "Account not found" unless @account
-    end
+    six_months_ago = (Time.now.utc - 6.months).beginning_of_month
+    start_date = six_months_ago
+    end_date = Time.now.utc
+    base_request_params = {
+      account: @account,
+      start_date: start_date,
+      end_date: end_date,
+      granularity: Constants::DAILY
+    }
 
-    def update
-        @account = current_user.accounts.find { |account| account.id == params[:id] }
+    # TODO: figure out how to do more async
+    @cost_summary = CostSummarizer.summarize(account: @account)
+    @csp_coverage = SavingsPlansFetcher.fetch_coverage(**base_request_params)
+    @csp_utilization = SavingsPlansFetcher.fetch_utilization(**base_request_params)
+    @on_demand_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ELIGIBLE_COST_AND_USAGE_FILTER)
+    @csp_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ONLY_USAGE_FILTER)
+    @historical_usage_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::EXCLUDE_IGNORED_SERVICES_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
+    @historical_usage_non_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::IGNORED_SERVICES_ONLY_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
+    # Async do |task|
+    #     task.async {
+    #         @csp_coverage = SavingsPlansFetcher.fetch_coverage(**base_request_params)
+    #     }
+    #     task.async {
+    #         @csp_utilization = SavingsPlansFetcher.fetch_utilization(**base_request_params)
+    #     }
+    #     task.async {
+    #         @on_demand_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ELIGIBLE_COST_AND_USAGE_FILTER)
+    #     }
+    #     task.async {
+    #         @csp_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ONLY_USAGE_FILTER)
+    #     }
+    #     task.async {
+    #         @historical_usage_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::EXCLUDE_IGNORED_SERVICES_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
+    #     }
+    #     task.async {
+    #         @historical_usage_non_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::IGNORED_SERVICES_ONLY_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
+    #     }
+    # end
+  end
 
-        if @account.update(account_params)
-          redirect_to account_analyses_path(@account)
-        else
-          render :edit, status: :unprocessable_entity
-        end
-    end
+  private
 
-    def show
-        @account = current_user.accounts.find { |account| account.id == params[:id] }
-        raise "Account not found" unless @account
-
-        six_months_ago = (Time.now.utc - 6.months).beginning_of_month
-        start_date = six_months_ago
-        end_date = Time.now.utc
-        base_request_params = {
-          account: @account,
-          start_date: start_date,
-          end_date: end_date,
-          granularity: Constants::DAILY
-        }
-
-        # TODO: figure out how to do more async
-        @cost_summary = CostSummarizer.summarize(account: @account)
-        @csp_coverage = SavingsPlansFetcher.fetch_coverage(**base_request_params)
-        @csp_utilization = SavingsPlansFetcher.fetch_utilization(**base_request_params)
-        @on_demand_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ELIGIBLE_COST_AND_USAGE_FILTER)
-        @csp_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ONLY_USAGE_FILTER)
-        @historical_usage_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::EXCLUDE_IGNORED_SERVICES_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
-        @historical_usage_non_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::IGNORED_SERVICES_ONLY_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
-        # Async do |task|
-        #     task.async {
-        #         @csp_coverage = SavingsPlansFetcher.fetch_coverage(**base_request_params)
-        #     }
-        #     task.async {
-        #         @csp_utilization = SavingsPlansFetcher.fetch_utilization(**base_request_params)
-        #     }
-        #     task.async {
-        #         @on_demand_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ELIGIBLE_COST_AND_USAGE_FILTER)
-        #     }
-        #     task.async {
-        #         @csp_usage = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::CSP_ONLY_USAGE_FILTER)
-        #     }
-        #     task.async {
-        #         @historical_usage_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::EXCLUDE_IGNORED_SERVICES_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
-        #     }
-        #     task.async {
-        #         @historical_usage_non_core = CostAndUsageFetcher.fetch(**base_request_params, filter: Constants::IGNORED_SERVICES_ONLY_FILTER, group_by: Constants::SERVICE, granularity: Constants::MONTHLY)
-        #     }
-        # end
-    end
-
-    private
-
-    def account_params
-        params.require(:account).permit(:name, :iam_access_key_id, :iam_secret_access_key, :role_arn)
-    end
+  def account_params
+    params.require(:account).permit(:name, :iam_access_key_id, :iam_secret_access_key, :role_arn)
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,22 @@
 class ApplicationController < ActionController::Base
-    protected
+  protected
 
-    def not_found!
-      raise ActionController::RoutingError, 'Not Found'
-    end
-  
-    def after_sign_in_path_for(resource)
-      stored_location_for(resource) || root_path
-    end
-  
-    def after_sign_out_path_for(_)
-      new_user_session_path
-    end
+  def not_found!
+    raise ActionController::RoutingError, 'Not Found'
+  end
 
-    private
-    def load_account!
-      @account = current_user.accounts.find { |account| account.id == params[:account_id] }
-      raise "Account not found" unless @account
-    end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(_)
+    new_user_session_path
+  end
+
+  private
+
+  def load_account!
+    @account = current_user.accounts.find { |account| account.id == params[:account_id] }
+    raise "Account not found" unless @account
+  end
 end
